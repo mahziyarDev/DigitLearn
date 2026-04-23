408e9a72175be937bc3e5710e7adf6f82b3c4e06
mahziyar dehghan <mahziyar138006@gmail.com>
2026-04-23 20:10:20 +0330

    add ticketModule

--- src/Modules/Blog/BlogModules/BlogBootstraper.cs
+++ src/Modules/Blog/BlogModules/BlogBootstraper.cs
@@ -1,30 +1,28 @@
 ﻿using BlogModules.Context;
 using BlogModules.Repository;
+using BlogModules.Service;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 
 namespace BlogModules
 {
     public static class BlogBootstraper
     {
-        public static IServiceCollection InitBlogModule(this IServiceCollection services,IConfiguration config)
+        public static IServiceCollection InitBlogModule(this IServiceCollection services, IConfiguration config)
         {
             services.AddDbContext<BlogContext>(option =>
             {
                 option.UseSqlServer(config.GetConnectionString("Blog_Context"));
             });
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IPostRepository, PostRepository>();
 
+            services.AddScoped<IBlogService, BlogService>();
 
-            //---Repository
-            services.AddTransient<IPostRepository, PostRepository>();
-            services.AddTransient<ICategoryRepository, CategoryRepository>();
-
-            //-----service
-            services.AddTransient<ICategoryRepository, CategoryRepository>();
-            services.AddTransient<IPostRepository, PostRepository>();
-
+            services.AddAutoMapper(conf => conf.AddProfile<MapperProfile>());
             return services;
         }
     }
---
+++ src/Modules/Ticket/TicketModule/TicketBootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using TicketModule.Core.Services;
+using TicketModule.Data;
+
+namespace TicketModule;
+
+public static class TicketBootstrapper
+{
+    public static IServiceCollection InitTicketModule(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<TicketContext>(option =>
+        {
+            option.UseSqlServer(config.GetConnectionString("Ticket_Context"));
+        });
+        services.AddScoped<ITicketService, TicketService>();
+
+
+        services.AddAutoMapper(conf => conf.AddProfile<MapperProfile>());
+
+        return services;
+    }
+}
